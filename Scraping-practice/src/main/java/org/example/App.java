940acd94ad627e9be625e60bd5cb73ea4c2598b3
AllanMD <allanavatar@hotmail.com>
2019-12-17 17:59:00 -0300

    restaurant scraping started

---
+++ Scraping-practice/src/main/java/org/example/App.java
@@ -0,0 +1,22 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public void getInfoByPage(String url){
+        nombre, direccion, direccion extendida(?), localidad, pais, telefono, puntuacion, rango de precios (precio minimo , y precio maximo), comidas, tipos de comida
+                email,
+
+        // buscar el script que tiene toda la info en el html, de ahi se saca toda la info. Hacer click derecho > copiar > selector de css
+    }
+
+
+}
