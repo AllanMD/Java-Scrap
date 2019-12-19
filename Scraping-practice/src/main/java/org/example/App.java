1d1ae6ef8688d6255d270494e1612c97f1e8d66b
AllanMD <allanavatar@hotmail.com>
2019-12-19 17:59:43 -0300

    single restaurant and restaurants of the entire page scraping finished

--- Scraping-practice/src/main/java/org/example/App.java
+++ Scraping-practice/src/main/java/org/example/App.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,7 +10,9 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import javax.swing.text.html.Option;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -20,66 +21,112 @@ import java.util.List;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static String BASE_URL = "https://www.tripadvisor.com.ar";
+
+    public static void main( String[] args ) throws Exception {
         System.out.println( "Hello World!" );
-        Restaurant restaurant =
-        getRestaurantInfoByPage("https://www.tripadvisor.com.ar/Restaurant_Review-g294305-d15801762-Reviews-Comedor_Central-Santiago_Santiago_Metropolitan_Region.html");
+        /*Restaurant restaurant =
+        getRestaurantByUrl("https://www.tripadvisor.com.ar/Restaurant_Review-g294305-d15801762-Reviews-Comedor_Central-Santiago_Santiago_Metropolitan_Region.html");
+        System.out.println(restaurant);*/
+
+        /*
+        Restaurant r2 =
+                getRestaurantInfoByPage("https://www.tripadvisor.com.ar/Restaurant_Review-g294305-d13137499-Reviews-Holy_Moly-Santiago_Santiago_Metropolitan_Region.html");
+
+        System.out.println(r2); */
+        //getRestaurantByUrl("https://www.tripadvisor.com.ar/Restaurant_Review-g294305-d10021953-Reviews-Uncle_Fletch_Plaza_Nunoa-Santiago_Santiago_Metropolitan_Region.html");
+        getRestaurantsByPage("https://www.tripadvisor.com.ar/Restaurants-g294305-Santiago_Santiago_Metropolitan_Region.html");
     }
+
 
     /**
      * To get the info of a restaurant of the page "tripadvisor.com.ar"
      * @param url
      * @return
+     * @throws Exception
      */
-    public static Restaurant getRestaurantInfoByPage(String url){
+    public static Restaurant getRestaurantByUrl(String url) throws Exception {
+        //we have to find the script that contains all the info of the restaurant in the HTML of the page.
+        // Tip: right click over a tag > copy > css selector
+        System.out.println(url);
         String jsonString = getJsonFromPage(url);
-        JsonNode jsonObject = getJsonObjectByString(jsonString);
+        System.out.println("JSON: " + jsonString);
+
+        if (jsonString.isEmpty()){
+            throw new Exception("Could not get json from the page");
+        }
+        JsonNode jsonObject = getJsonObjectByString(jsonString); // check that jsonobject isnt null
+        if (jsonObject.isNull()){
+            throw new Exception();
+        }
         String restaurant_id = getRestaurantId(url);
-        jsonObject = jsonObject.get("api").get("responses").get("/data/1.0/location/"+ restaurant_id);
-        System.out.println("Restauran id: " + restaurant_id);
+        if (restaurant_id.isEmpty()){
+            throw new Exception("restaurant id empty");
+        }
 
-        String name = jsonObject.get("data").get("name").asText();
-        System.out.println("NOMBRE DE RESTAURANTE: " + name);
+        JsonNode restaurantDetails = jsonObject.get("api").get("responses").get("/data/1.0/location/"+ restaurant_id);
+        JsonNode restaurantOverview = jsonObject.get("api").get("responses").get("/data/1.0/restaurant/"+restaurant_id+"/overview").get("data"); //another part of the JSON that also contains info
 
-        String address = jsonObject.get("data").get("address_obj").get("street1").asText();
-        System.out.println("Direccion: " + address);
+        String name = getNameFromJson(restaurantDetails);
 
-        String extendedAddress = jsonObject.get("data").get("address_obj").get("street2").asText();
-        System.out.println("Direccion extendida: " + extendedAddress);
+        String address = getAddressFromJson(restaurantDetails);
 
-        String location = jsonObject.get("data").get("location_string").asText();
-        System.out.println("location: " + location);
+        String extendedAddress = getExtendedAddressFromJson(restaurantDetails);
 
-        String country = jsonObject.get("data").get("address_obj").get("country").asText();
-        System.out.println("country: " + country);
+        String location = getLocationFromJson(restaurantDetails);
 
-        String phone = jsonObject.get("data").get("phone").asText();
-        System.out.println("phone: " + phone);
+        String country = getCountryFromJson(restaurantDetails);
 
-        String score = jsonObject.get("data").get("rating").asText();
-        System.out.println("score: " + score);
+        String phone = getPhoneFromJson(restaurantDetails);
 
-        String price = jsonObject.get("data").get("price").asText();
-        System.out.println("price: " + price);
+        String score = getScoreFromJson(restaurantDetails);
 
-        // List<String> meals = jsonObject.get("data").get() // meals y mealstypes estan en otra parte del JSON
+        String price = getPriceFromJson(restaurantDetails);
 
-        String email = jsonObject.get("data").get("email").asText();
-        System.out.println("email: " + email);
+        List<String> cuisine = getCuisineFromJson(restaurantDetails);
 
-        return new Restaurant();
+        // meals are in another part of the JSON
+        List<String> meals = getMealsFromJson(restaurantOverview);
 
-        // buscar el script que tiene toda la info en el html, de ahi se saca toda la info. Hacer click derecho > copiar > selector de css
+        String email = getEmailFromJson(restaurantDetails);
+
+        String website = getWebsiteFromJson(restaurantDetails);
+
+        Restaurant restaurant = new Restaurant(name,address,extendedAddress,location,country,phone,score,price,meals,cuisine,email,website);
+
+        return restaurant;
     }
 
     public static String getJsonFromPage(String url){
         String jsonString = "";
         try {
-            Document doc = Jsoup.connect(url).get();
-            Element body = doc.selectFirst("#BODY_BLOCK_JQUERY_REFLOW");  // trae toda la pagina entera, de ahi se busca el JSON que contiene todos los datos importantes
-             String json = body.toString();
-            String from = "\"api\":"; // https://www.lawebdelprogramador.com/foros/Java/596168-introduccion-de-comillas-en-un-string-java.html
+            Document doc = Jsoup.connect(url) // https://github.com/jhy/jsoup/issues/287 , to solve the problem that certain times jsoup didnt bring all the HTML
+                    .header("Accept-Encoding", "gzip, deflate")
+                    .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0")
+                    .maxBodySize(0) // jsoup has a default maxBodySize and thats why i received an uncomplete body
+                    .timeout(600000)
+                    .get();
+            Element body = doc.selectFirst("#BODY_BLOCK_JQUERY_REFLOW");  // trae toda la pagina entera, de ahi se busca el JSON que contiene todos los datos importantes // hay que traer toda la pagina, ya que la parte que contiene el JSON cambia de ubicacion en cada peticion para evitar scraping (?)
+            //intentar pasar la parte de arriba a solo seleccionar los script de la pagina, usar stream y filtrar por el que contenga "window.__WEB_CONTEXT_"
+
+
+            /* // TERMINAR DE PASAR ESTO, asignar esto a body (cambiar nombre de body a bodyString o scriptString)
+            var scriptBodyOptional = doc.getElementsByTag("script").stream()
+                    .map(Element::toString)
+                    .filter(scriptBody -> scriptBody.contains("window.__WEB_CONTEXT_"))
+                    .findAny();
+                    // getElementsByTag: Elements
+                    // .stream : Stream<Element>
+                    // ? : Stream<String>
+                    // .filter : Stream<Element>
+                    // .collect : Element
+
+            */
+
+
+            String json = body.toString();
+
+             String from = "\"api\":"; // https://www.lawebdelprogramador.com/foros/Java/596168-introduccion-de-comillas-en-un-string-java.html
             // redux ---> api -->  responses       ----> "/data/1.0/location/15801762" ??? es la url de la api
             String to = "\"page\":";
             int indexFrom = json.indexOf(from); //https://typingcode.wordpress.com/2013/03/19/manejo-de-strings-en-java/
@@ -88,9 +135,6 @@ public class App
                 jsonString = json.substring(indexFrom, indexTo-1);
                 jsonString = "{" + jsonString + "}"; // formatear correctamente el json. validar en: https://jsonformatter.curiousconcept.com/
             }
-
-
-
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -101,7 +145,7 @@ public class App
         ObjectMapper JSON_MAPPER = new ObjectMapper();
         JsonNode jsonObject = null;
         try {
-            jsonObject = JSON_MAPPER.readValue(json, JsonNode.class); // json node es el equivalente a JSON Object de Gson.
+            jsonObject = JSON_MAPPER.readValue(json, JsonNode.class); //JsonNode is the equivalent to the JSON Object from Gson.
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
         } catch (JsonParseException e) {
@@ -129,7 +173,162 @@ public class App
         return id;
     }
 
+    /* para mejorar el codigo, para mas adelante
+    public static JsonNode getRestaurantDetailsById(JsonNode jsonObject){
+
+    }
+
+    public static JsonNode getRestaurantOverviewById(JsonNode jsonObject){
+
+    }
+    */
 
 
+    public static List<Restaurant> getRestaurantsByPage(String url) throws Exception {
+        String jsonString = getJsonFromPage(url);
+        if (jsonString.isEmpty()){
+            throw new Exception("Json empty");
+        }
+
+        JsonNode jsonObject = getJsonObjectByString(jsonString);
+        if (jsonObject == null){
+            throw new Exception("json object null");
+        }
+
+        JsonNode data = jsonObject.get("api").get("responses");//.get("data").get("restaurants");
+        //data/1.0/restaurants/294305/14785102,12950301,2410245,7755054,777610,14012119,1735271,15184025,799339,10021953,6638542,8494943,10761466,7312299,15248197,7161213,10503885,3843350,15247797,3838800,2311987,6529842,1493782,6131405,12701486,2410244,8755441,7291241,3547430,940625,5292694?tags=&reviewStubInfo=true&sponsoredLocationIndices=0     ---> the name of the part that haves all the restaurants of the page
+        String json = data.toString();
+        int indexFrom = json.indexOf("/data/1.0/restaurants/"); // when me use substring, the resulting string will contain this: "/data/1.0/restaurants/"
+        int indexTo = json.indexOf("?tags=&reviewStubInfo=true&sponsoredLocationIndices=0");
+        String idsUrl = json.substring(indexFrom, indexTo);
+
+        data = data.get( idsUrl + "?tags=&reviewStubInfo=true&sponsoredLocationIndices=0").get("data").get("restaurants");
+
+        List<String> urls = getPagesUrls(data);
+        System.out.println("urls size: " + urls.size());
+        List<Restaurant> restaurants = new ArrayList<Restaurant>();
+        for (int i = 0; i < urls.size(); i++) {
+            restaurants.add(getRestaurantByUrl(urls.get(i)));
+
+        }
+
+        System.out.println(restaurants);
+        return restaurants;
+    }
+
+    public static List<String> getPagesUrls(JsonNode data){
+        List<String> urls = new ArrayList<>();
+        for (int i = 0; i < data.size(); i++) {
+            String pageUrl = BASE_URL + data.get(i).get("detailPageUrl").asText();
+            urls.add(pageUrl);
+        }
+        return urls;
+    }
+    // ------ The methods here below are for getting info from the restaurant json ------
+    public static String getNameFromJson(JsonNode restaurantDetails){
+        String name = ""; //to return it empty if the data doesnt exist in the JSON
+
+        if (restaurantDetails.get("data").get("name") != null){
+            name = restaurantDetails.get("data").get("name").asText();
+        }
+
+        return name;
+    }
+
+    public static String getAddressFromJson(JsonNode restaurantDetails){
+        String address = "";
+        if (restaurantDetails.get("data").get("address_obj").get("street1") != null){
+            address = restaurantDetails.get("data").get("address_obj").get("street1").asText();
+        }
+        return address;
+    }
+
+    public static String getExtendedAddressFromJson(JsonNode restaurantDetails){
+        String extendedAddress = "";
+        if (restaurantDetails.get("data").get("address_obj").get("street2") != null){
+            extendedAddress = restaurantDetails.get("data").get("address_obj").get("street2").asText();
+        }
+        return extendedAddress;
+    }
+
+    public static String getLocationFromJson(JsonNode restaurantDetails){
+        String location = "";
+        if (restaurantDetails.get("data").get("location_string") != null){
+            location = restaurantDetails.get("data").get("location_string").asText();
+        }
+        return location;
+    }
+
+    public static String getCountryFromJson(JsonNode restaurantDetails){
+        String country = "";
+        if (restaurantDetails.get("data").get("address_obj").get("country") != null){
+            country = restaurantDetails.get("data").get("address_obj").get("country").asText();
+        }
+        return country;
+    }
+
+    public static String getPhoneFromJson(JsonNode restaurantDetails){
+        String phone = "";
+        if (restaurantDetails.get("data").get("phone") != null){
+            phone = restaurantDetails.get("data").get("phone").asText();
+        }
+        return phone;
+    }
+
+    public static String getScoreFromJson(JsonNode restaurantDetails){
+        String score = "";
+        if (restaurantDetails.get("data").get("rating") != null){
+            score = restaurantDetails.get("data").get("rating").asText();
+        }
+        return score;
+    }
+
+    public static String getPriceFromJson(JsonNode restaurantDetails){
+        String price = "";
+        if (restaurantDetails.get("data").get("price") != null){
+            price = restaurantDetails.get("data").get("price").asText();
+        }
+        return price;
+    }
+
+    public static List<String> getCuisineFromJson(JsonNode restaurantDetails){
+        List<String> cuisine = new ArrayList<String>();
+        if (restaurantDetails.get("data").get("cuisine") != null){
+            for (JsonNode node : restaurantDetails.get("data").get("cuisine")){ //the meals are inside an array named "cuisine"l
+                cuisine.add(node.get("name").asText());
+            }
+        }
+        return cuisine;
+    }
+
+    public static List<String> getMealsFromJson(JsonNode restaurantOverview){
+        List<String> meals = new ArrayList<String>();
+        if (restaurantOverview.get("detailCard").get("tagTexts").get("meals").get("tags") != null){
+            for (JsonNode node : restaurantOverview.get("detailCard").get("tagTexts").get("meals").get("tags")){
+                meals.add(node.get("tagValue").asText());
+            }
+        }
+        return meals;
+    }
+
+    public static String getEmailFromJson(JsonNode restaurantDetails){
+        String email = "";
+
+        if (restaurantDetails.get("data").get("email") != null){
+            email = restaurantDetails.get("data").get("email").asText();
+        }
+
+        return email;
+    }
+
+    public static String getWebsiteFromJson(JsonNode restaurantDetails){
+        String website = "";
+
+        if (restaurantDetails.get("data").get("website") != null){
+            website = restaurantDetails.get("data").get("website").asText();
+        }
+
+        return website;
+    }
 
 }
--- Scraping-practice/src/main/java/org/example/domain/Restaurant.java
+++ Scraping-practice/src/main/java/org/example/domain/Restaurant.java
@@ -1,6 +1,7 @@
 package org.example.domain;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Restaurant {
     private String name;
@@ -11,10 +12,139 @@ public class Restaurant {
     private String phone;
     private String score;
     private String price;
-    private String min_price; //  delete ?
-    private String max_price; // delete ?
-    private ArrayList<String> meals;
-    private ArrayList<String>mealsTypes;
+    //private String min_price; //  delete ?
+    //private String max_price; // delete ?
+    private List<String> meals;
+    private List<String>cuisine;
     private String email;
+    private String website;
 
+    public Restaurant(String name, String address, String extendedAddress, String location, String country, String phone, String score, String price, List<String> meals, List<String> cuisine, String email, String website) {
+        this.name = name;
+        this.address = address;
+        this.extendedAddress = extendedAddress;
+        this.location = location;
+        this.country = country;
+        this.phone = phone;
+        this.score = score;
+        this.price = price;
+        this.meals = meals;
+        this.cuisine = cuisine;
+        this.email = email;
+        this.website = website;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getExtendedAddress() {
+        return extendedAddress;
+    }
+
+    public void setExtendedAddress(String extendedAddress) {
+        this.extendedAddress = extendedAddress;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public List<String> getMeals() {
+        return meals;
+    }
+
+    public void setMeals(List<String> meals) {
+        this.meals = meals;
+    }
+
+    public List<String> getCuisine() {
+        return cuisine;
+    }
+
+    public void setCuisine(List<String> cuisine) {
+        this.cuisine = cuisine;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    @Override
+    public String toString() {
+        return "Restaurant{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", extendedAddress='" + extendedAddress + '\'' +
+                ", location='" + location + '\'' +
+                ", country='" + country + '\'' +
+                ", phone='" + phone + '\'' +
+                ", score='" + score + '\'' +
+                ", price='" + price + '\'' +
+                ", meals=" + meals +
+                ", cuisine=" + cuisine +
+                ", email='" + email + '\'' +
+                ", website='" + website + '\'' +
+                '}';
+    }
 }
